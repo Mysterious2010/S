f550b518edce14dfaa5a7d367e245224b8815d2f
Mysterious2010 <95913199+Mysterious2010@users.noreply.github.com>
2022-06-22 16:39:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+difference=0;
+rightWrist=0;
+leftWrist=0;
+
+function setup(){
+    video=createCapture(VIDEO)
+    video.size(450,400);
+
+    canvas=createCanvas(450,400);
+    canvas.position(560,100);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Is Initialized!");
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+leftWristX=results[0].pose.leftWrist.x;
+rightWristX=results[0].pose.rightWrist.x;
+difference=floor(leftWristX-rightWristX);
+
+console.log("leftWristX="+leftWristX+"rightWristX +" +rightWristX+"difference+"+difference);
+    }
+}
+ function draw(){
+    background('#6C91C2');
+    document.getElementById("font_size").innerHTML="Font size of the text will be+"+difference+"px";
+    textSize(difference);
+    fill('#FFE787');
+    text('Saanvi',50,400);
+ }
